2c7615b4298768a8c4c6246b2ea1f3f8bb5eff41
Roan Hofland <roan@roanh.dev>
2021-12-02 12:47:25 +0100

    Day 2 Silver Star

---
+++ AdventOfCode2021/src/dev/roanh/aoc/day02/Main.java
@@ -0,0 +1,43 @@
+package dev.roanh.aoc.day02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main{
+	private static List<String> lines;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream("dev/roanh/aoc/day02/input")));
+		lines = in.lines().collect(Collectors.toList());
+		in.close();
+		
+		silverStar();
+	}
+
+	private static void silverStar(){
+		int dist = 0;
+		int depth = 0;
+		
+		for(String line : lines){
+			String[] args = line.split(" ");
+			switch(args[0]){
+			case "forward":
+				dist += Integer.parseInt(args[1]);
+				break;
+			case "down":
+				depth += Integer.parseInt(args[1]);
+				break;
+			case "up":
+				depth -= Integer.parseInt(args[1]);
+				break;
+			default:
+				throw new RuntimeException("What?");
+			}
+		}
+		
+		System.out.println("Silver star: " + (dist * depth));
+	}
+}
